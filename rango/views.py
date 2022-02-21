03705ea700c7858b0701a0970656cd3dd57ea982
Emiriaa <751198482@qq.com>
2022-02-21 13:56:09 +0000

    chapter3 test completed

--- rango/views.py
+++ rango/views.py
@@ -1,27 +1,7 @@
-from cgitb import html
-from email.quoprimime import body_check
-from re import A
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request,'rango/index.html',context = context_dict)
-
-def about(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'This tutorial has been put together by Yunyi Wang'}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request,'rango/about.html',context = context_dict)
+def index(response):
+    return HttpResponse("Rango says hey there partner!")
